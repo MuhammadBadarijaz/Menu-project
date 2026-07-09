c3605e060cb3a35463f6cb912ebfb1383597c558
MuhammadBadarijaz <baddergujjar@gmail.com>
2026-07-09 15:35:30 -0700

    Create Category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const Categories = ({filteritems}) => {
+const Categories = ({ filteritems }) => {
   return (
-    <div className='flex justify-center gap-3 m-8'>
-        <button  
-        onClick={()=> filteritems('All')}
-        className='bg-yellow-500 text-white px-3 py-1 rouned'>
-            All
-        </button>
-        <button
-                onClick={()=> filteritems('breakfast')}
-
-         className='bg-yellow-500 text-white px-3 py-1 rouned'>
-            Breakfast
-        </button>
-        <button 
-                onClick={()=> filteritems('lunch')}
-
-        className='bg-yellow-500 text-white px-3 py-1 rouned'>
-            Lunch
-        </button>
-        <button
-                onClick={()=> filteritems('shakes')}
-
-         className='bg-yellow-500 text-white px-3 py-1 rouned'>
-            Shakes
-        </button>
+    <div className="flex justify-center gap-3 m-8">
+      <button
+        onClick={() => filteritems("All")}
+        className="bg-yellow-500 text-white px-3 py-1 rouned"
+      >
+        All
+      </button>
+      <button
+        onClick={() => filteritems("breakfast")}
+        className="bg-yellow-500 text-white px-3 py-1 rouned"
+      >
+        Breakfast
+      </button>
+      <button
+        onClick={() => filteritems("lunch")}
+        className="bg-yellow-500 text-white px-3 py-1 rouned"
+      >
+        Lunch
+      </button>
+      <button
+        onClick={() => filteritems("shakes")}
+        className="bg-yellow-500 text-white px-3 py-1 rouned"
+      >
+        Shakes
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
